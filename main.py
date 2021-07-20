80a49a35246bf20f6f9867f956045d44a5203b23
The Sage <83530047+Raj6695@users.noreply.github.com>
2021-07-20 11:46:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ onlyfiles = [f for f in listdir("static/assets/img/post_img") if isfile(join("st
 
 app = Flask(__name__)
 
-# response = requests.get("https://api.npoint.io/88c2c1f644ef334058be")
-# data = response.json()
+OWN_EMAIL =  EMAIL ADDRESS
+OWN_PASSWORD = PASSWORD
 
 blog = 0
 
@@ -32,6 +32,27 @@ def index():
 def about():
     return render_template("about.html")
 
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
